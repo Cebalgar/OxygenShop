6b7b7407ce699cc2a098cd62d3c5e313961dc0fc
Celia Ballesteros <cebalgar@gmail.com>
2022-08-08 16:45:09 +0200

    button Return Top

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const btnMenu= document.getElementById("btn");
 
 const showMenu = document.getElementById("links");
 
-var handleClick = function(event) {
+const handleClick = function(event) {
  showMenu.classList.toggle("show");
  btnMenu.classList.toggle("remove");
  //console.log("HOLA");
@@ -13,15 +13,34 @@ var handleClick = function(event) {
 btnMenu.addEventListener("click", handleClick);
 
 
+
 // percentage scroller
 
 function handleScroll(){
-    let winScroll = document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const winScroll = document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight; //altura mínima(incluye altura relleno, pero no bordes y márgenes) - la altura que incluye el padding.
     let scrolled = (winScroll/height)*100;
 
     document.getElementById("bar").style.width=scrolled + "%";
 }
 
 document.addEventListener("scroll", handleScroll);
+
+
+
+// botón "Return to the Top"
+
+const btnTop = document.getElementById("returnBtn");
+
+const handleTop = () => {
+    window.scrollTo( { //scrollTo permite navegar a una posición del scroll
+        top:0, 
+        behavior:"smooth"
+    });
+    setTimeout(handleTop, 200);
+};
+
+btnTop.addEventListener("click",handleTop);
+
+
 
