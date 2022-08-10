9b772c74a46e2544b97cdaa5aecdc5c0bc0e42a1
Celia Ballesteros <cebalgar@gmail.com>
2022-08-10 15:48:25 +0200

    button return top and POST form data

--- main.js
+++ main.js
@@ -32,15 +32,86 @@ document.addEventListener("scroll", handleScroll);
 
 const btnTop = document.getElementById("returnBtn");
 
-const handleTop = () => {
-    window.scrollTo( { //scrollTo permite navegar a una posición del scroll
-        top:0, 
-        behavior:"smooth"
-    });
-    setTimeout(handleTop, 200);
-};
+function handleTop(){
+    setTimeout(function(){
+        window.scrollTo({ top:0, behavior:"smooth"})
+},200);
+
+}
 
 btnTop.addEventListener("click",handleTop);
+
+
+ //Implementar validación al formulario
+const nameInput = document.getElementById("name");
+const mailInput = document.getElementById("mail");
+const checkboxInput = document.getElementById("cbox1");
+const form= document.getElementById("form");
+const error = document.getElementsByClassName("regist");
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    let regexEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/;
+    
+    
+    if(nameInput.value.length < 2 || nameInput.value.length >100){
+      alert("nombre NO VALIDO");
+      
+     
+    }
+    if(!regexEmail.test(mail.value)){
+        alert ("el email no es valido")
+    }
+  
+    if(!checkboxInput.checked){
+        alert ("checkbox NO esta selecionado")
+    }
+    
+});
+
+//Recoger datos formulario en servidor
+
+const formulario = document.getElementById("form");
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+    const InputName = document.getElementById("name").value;
+    const InputMail = document.getElementById("mail").value;
+
+    const datos = {
+        nombre: InputName, 
+        mail: InputMail}     
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify(datos),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+  })
+
+  .then((response) => response.json())
+  .then(result => {
+    console.log(datos);
+  })
+  .catch((error)=>{
+    console.log ("Error en la petición: " + error);
+  })
+      
+})
+// Crear popup (/modal)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
